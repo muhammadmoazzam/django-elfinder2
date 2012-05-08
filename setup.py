d15a1f7bea67b2ff83268dace7dc8f4c506ef6b7
Davide Silvestri <silvestridavide87@gmail.com>
2012-05-08 13:46:28 +0200

    added MANIFEST.in and changed setup.py to copy templates and static files

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-#!/usr/bin/env python
+# econding=utf8
 
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
 
-setup(name='django-elfinder',
-      version='0.2',
-      description='Django connector for elFinder 2',
-      author='Mike Ryan',
-      author_email='mike@fadedink.co.uk',
-      url='https://github.com/mikery/django-elfinder/',
-      download_url='https://github.com/mikery/django-elfinder/tarball/v0.2',
-      packages=['elfinder', 'elfinder.volume_drivers'],
-      requires=['django (>=1.3)', 'mptt (>=0.5.2)'],
-      )
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+README = read('README.rst')
+
+setup(
+    name = 'django-elfinder',
+    version = '0.2',
+    description = 'Django connector for elFinder 2',
+    long_description = README,
+    author = 'Mike Ryan',
+    author_email = 'mike@fadedink.co.uk',
+    url = 'https://github.com/mikery/django-elfinder/',
+    download_url = 'https://github.com/mikery/django-elfinder/tarball/v0.2',
+    packages = find_packages(),
+    include_package_data=True,
+    requires = ['django (>=1.3)', 'mptt (>=0.5.2)'],
+)
