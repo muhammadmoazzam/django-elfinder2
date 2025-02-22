fc0cec6e6fbe2412cc310bcb5c561b1f9677bece
Muhammad Moazzam <21063329+muhammadmoazzam@users.noreply.github.com>
2025-02-22 20:00:49 +0500

    Update for Django 2.0+

--- elfinder/urls.py
+++ elfinder/urls.py
@@ -1,14 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
 from elfinder.views import index, connector_view
 from elfinder.views_tinymce import tinymce_filebrowser_script_view, tinymce_filebrowser_dialog_view
 
 urlpatterns = [
-    url(r'^(?P<coll_id>\d+)/$', index, name='elfinder_index'),
-    url(r'^connector/(?P<coll_id>\d+)/$', connector_view, name='elfinder_connector'),
-
-    url(r'^$', index, name='elfinder_index'),
-    url(r'^connector/$', connector_view, name='elfinder_connector'),
-
-    url(r'^tinymce/filebrowser-script/$', tinymce_filebrowser_script_view, name='elfinder_tinymce_filebrowser_script'),
-    url(r'^tinymce/filebrowser-dialog/$', tinymce_filebrowser_dialog_view, name='elfinder_tinymce_filebrowser_dialog'),
+   path('<int:coll_id>/', index, name='elfinder_index'),
+   path('connector/<int:coll_id>/', connector_view, name='elfinder_connector'),
+   path('', index, name='elfinder_index'),
+   path('connector/', connector_view, name='elfinder_connector'),
+   path('tinymce/filebrowser-script/', tinymce_filebrowser_script_view, name='elfinder_tinymce_filebrowser_script'),
+   path('tinymce/filebrowser-dialog/', tinymce_filebrowser_dialog_view, name='elfinder_tinymce_filebrowser_dialog'),
 ]
